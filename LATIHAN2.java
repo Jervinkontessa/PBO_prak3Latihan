02485998f6de836ca8eb7f103dabedd68bc70f95
Jervin descrates kontessa <71800787+Jervinkontessa@users.noreply.github.com>
2020-10-08 11:12:12 +0800

    Add files via upload

---
+++ LATIHAN2.java
@@ -0,0 +1,17 @@
+class LATIHAN2 {
+	public static void main(String[] args){
+
+	char  A='A'; 
+	char   B ='B'; 
+	char C= '\u0063';
+	String kata="\\U0061R\\U0062\\U0063";
+	
+		
+		
+System.out.println("a: "+A);
+System.out.println("b; " +B );
+System.out.println("c: " + C);
+System.out.println("kata: " + kata);
+
+  }
+}
---
+++ latihan4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class latihan4{
+	public static void main(String[] args){
+		Scanner input =new Scanner(System.in);
+		int var_a,var_b;
+		System.out.print("masukkan nilai var var_a:");
+		var_a = input.nextInt();
+		System.out.print("masukkan nilai var var_b :");
+		var_b = input.nextInt();
+		System.out.println();
+		System.out.println();
+		System.out.println("Variabel yang terdapat didalam program :");
+		System.out.println("var_a = "+var_a);
+		System.out.println("var_b = "+var_b);
+	}
+}
